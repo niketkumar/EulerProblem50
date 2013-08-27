46e3d8ebf077b05291c26a2f6d0cc0d0dc38ba10
niket <niketkumar@gmail.com>
2013-08-27 19:24:59 +0530

    fixing typo

--- src/main/scala/eulerProblem50/Problem50.scala
+++ src/main/scala/eulerProblem50/Problem50.scala
@@ -2,6 +2,6 @@ package eulerProblem50
 
 object Problem50 {
   def solution(limit: Int) = {
-    (new LongestSum4 with ESieve).solution(limit)
+    (new LongestSum with ESieve).solution(limit)
   }
 }
